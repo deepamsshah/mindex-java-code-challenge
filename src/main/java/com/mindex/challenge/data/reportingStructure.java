a26fdd2c0d4f314f4612977812488ac828f2e720
deepamsshah <deepam25498@gmail.com>
2021-01-23 19:52:30 -0500

    added reportingStructure.java

--- src/main/java/com/mindex/challenge/data/reportingStructure.java
+++ src/main/java/com/mindex/challenge/data/reportingStructure.java
@@ -2,5 +2,18 @@ package com.mindex.challenge.data;
 
 public class reportingStructure {
     private int numberOfReports;
+    private Employee employee;
 
+    public int getNumberOfReports() {
+        return numberOfReports;
+    }
+    public void setNumberOfReports(int numberOfReports) {
+        this.numberOfReports = numberOfReports;
+    }
+    public Employee getEmployee() {
+        return employee;
+    }
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
 }
